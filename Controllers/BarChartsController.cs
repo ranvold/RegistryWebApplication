728c81072da99a09b33d5a87edb8cf24dcafacb8
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2022-05-21 18:22:55 +0300

    1.5

---
+++ Controllers/BarChartsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegistryWebApplication.Models;
+
+namespace RegistryWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarChartsController : ControllerBase
+    {
+        private readonly DBRegistryContext _context;
+
+        public BarChartsController(DBRegistryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonDataBarChart")]
+        public JsonResult JsonDataBarChart()
+        {
+
+            var totalWorks = _context.Works.Count();
+            var defensedWorks = _context.Works.Count(d => d.Mark >= 60);
+
+            List<object> works = new List<object>();
+            works.Add(new[] { "Works", "Total", "Defensed" });
+
+            works.Add(new object[] { "Works", totalWorks, defensedWorks });
+
+            return new JsonResult(works);
+        }
+    }
+}
---
+++ Controllers/ChartsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegistryWebApplication.Models;
+
+namespace RegistryWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly DBRegistryContext _context;
+
+        public ChartsController(DBRegistryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonDataChart")]
+        public JsonResult JsonDataChart()
+        {
+            var teachers = _context.Teachers.ToList();
+            List<object> teacherWorks = new List<object>();
+            teacherWorks.Add(new[] { "Teacher", "Works" });
+
+            foreach (var t in teachers)
+            {
+                var workCount = _context.Works.Count(w => w.TeacherId == t.Id);
+
+                teacherWorks.Add(new object[] { t.LastName + " " + t.FirstName + " " + t.FathersName, workCount });
+            }
+            return new JsonResult(teacherWorks);
+        }
+
+
+    }
+
+}
